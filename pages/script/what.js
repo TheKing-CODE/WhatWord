5b5909f447035ee1aaab4f7b9b40225453bdda36
Allan Christian <143041480+TheKing-CODE@users.noreply.github.com>
2024-09-30 21:25:44 -0300

    Update
    Consulta e verificação de palavras.
    Preenchimento do layout.

--- pages/script/what.js
+++ pages/script/what.js
@@ -1,24 +1,49 @@
 $(function(){
 	let numeroAleatorio = numeroAleatorioEntre(0,9);
 
+	var palavra = getPalavraAletoria();
+	var qntAcertos = 0;
+
+	preencherDadosPalavras(palavra);
+	
+	$('#pular-palavra').click(function(){
+		palavra = getPalavraAletoria();
+		if(palavra == undefined){
+			
+		}
+		preencherDadosPalavras(palavra);
+	})
 
 	$('#limpar-texto').click(function(){
-		$('#respota-texto').val(' ');
-		//$('#respota-texto').attr('placeholder','Qual a palavra?');
-	})
+		$('#respota-texto').val(' ');		
+	});
+
+	$('#resposta-texto').click(function(){
+		let palavraDigitada = $('#respota-texto').val();
+		if(palavra[0] == palavraDigitada){
+			$.notify("Certo", "success");
+			palavra = getPalavraAletoria();
+			preencherDadosPalavras(palavra);
+			qntAcertos++;
+			$('#qnt-acertos').text(qntAcertos);
+			$('#respota-texto').val(' ')
+		}else{
+			return false;
+		}		
+	});
 })
 
 
-	const PalavraUm = ['Hogwarts','Escola de magia da série "Harry Potter"', 'war-ts-hog'];
-	const PalavraDois = ['Bolseiro','Sobrenome de um personagem da trilogia de "O Hobbit"', 'ro-bol-sei'];
-	const PalavraTres = ['Gandalf', 'Mago icônico do universo de "O Senhor dos Anéis"', 'dalf-gan'];
-	const PalavraQuatro = ['Barcelona', 'Clube de futebol famoso da Espanha', 'lo-na-ce-bar'];
-	const PalavraCinco = ['Maradona', 'Jogador de futebol argentino, considerado um dos melhores da história', 'ra-ma-do-na'];
-	const PalavraSeis = ['Python', 'Linguagem de programação popular para ciência de dados', 'thon-py'];
-	const PalavraSete = ['JavaScript', 'Linguagem fundamental para desenvolvimento web', 't-va-scrip-ja'];
-	const PalavraOito = ['Sonic', 'Personagem famoso de jogos de vídeo game, conhecido como "Sonic the Hedgehog"', 'nic-so'];
-	const PalavraNove = ['Daenerys', 'Personagem central da série "Game of Thrones"', 'ne-e-da-rys'];
-	const PalavraDez = ['Neymar', 'Atacante brasileiro destacado no futebol mundial', 'mar-ney'];
+	const PalavraUm = ['hogwarts','Escola de magia da série "Harry Potter"', 'war-ts-hog'];
+	const PalavraDois = ['bolseiro','Sobrenome de um personagem da trilogia de "O Hobbit"', 'ro-bol-sei'];
+	const PalavraTres = ['gandalf', 'Mago icônico do universo de "O Senhor dos Anéis"', 'dalf-gan'];
+	const PalavraQuatro = ['barcelona', 'Clube de futebol famoso da Espanha', 'lo-na-ce-bar'];
+	const PalavraCinco = ['maradona', 'Jogador de futebol argentino, considerado um dos melhores da história', 'ra-ma-do-na'];
+	const PalavraSeis = ['python', 'Linguagem de programação popular para ciência de dados', 'thon-py'];
+	const PalavraSete = ['javaScript', 'Linguagem fundamental para desenvolvimento web', 't-va-scrip-ja'];
+	const PalavraOito = ['sonic', 'Personagem famoso de jogos de vídeo game, conhecido como "Sonic the Hedgehog"', 'nic-so'];
+	const PalavraNove = ['daenerys', 'Personagem central da série "Game of Thrones"', 'ne-e-da-rys'];
+	const PalavraDez = ['neymar', 'Atacante brasileiro destacado no futebol mundial', 'mar-ney'];
 
 	const palavrasCombinadas = [
 	    PalavraUm,
@@ -42,26 +67,25 @@ function numeroAleatorioEntre(min, max) {
 
 function getPalavraAletoria(){
 	var numeroSelecionado;
-	for (var i = 0; i < 10; i++) {
-		numeroSelecionado = numeroAleatorioEntre(0,9);
-		if(!numerosUtilizados.includes(numeroSelecionado)){
-			numerosUtilizados.push(numeroSelecionado);		
-			return palavrasCombinadas[numeroSelecionado];
+	if(numerosUtilizados.length() > 9){
+		return undefined;
+	}else{
+		for (var i = 0; i < 10; i++) {
+			numeroSelecionado = numeroAleatorioEntre(0,9);
+			if(!numerosUtilizados.includes(numeroSelecionado)){
+				numerosUtilizados.push(numeroSelecionado);		
+				return palavrasCombinadas[numeroSelecionado];
+			}
 		}
 	}
 };
 
+function preencherDadosPalavras(palavra_){
+	$('#palavra-embaralhada').text(palavra_[2]);
+	$('#dica-palavra').text(palavra_[1]);
+}
 
-/*Hogwarts: Escola de magia da série "Harry Potter".
-Frodo: Protagonista da trilogia "O Senhor dos Anéis".
-Gandalf: Mago icônico do mesmo universo de "O Senhor dos Anéis".
-Barcelona: Clube de futebol famoso da Espanha, conhecido mundialmente.
-Messi: Jogador de futebol considerado um dos melhores da história.
-Python: Linguagem de programação popular, usada em diversas áreas, como ciência de dados.
-JavaScript: Linguagem fundamental para desenvolvimento web e interatividade em sites.
-Sonic: Personagem famoso de jogos de vídeo game, conhecido como "Sonic the Hedgehog".
-Daenerys: Personagem central da série "Game of Thrones".
-Neymar: Atacante brasileiro destacado no futebol mundial.*/
+
 
 
 
