0043ed26212aec13fa7c0630e4e9011430157d7e
Allan Christian <143041480+TheKing-CODE@users.noreply.github.com>
2024-10-01 22:23:29 -0300

    Update

--- pages/script/what.js
+++ pages/script/what.js
@@ -3,15 +3,21 @@ $(function(){
 
 	var palavra = getPalavraAletoria();
 	var qntAcertos = 0;
+	var qntPalavrasPuladas = 0;
 
 	preencherDadosPalavras(palavra);
 	
-	$('#pular-palavra').click(function(){
-		palavra = getPalavraAletoria();
-		if(palavra == undefined){
-			
+	$('#pular-palavra').click(function(){		
+		qntPalavraUsadas = parseInt(getQntPalavrasUtilizadas());
+		console.log(qntPalavraUsadas);
+		if(qntPalavraUsadas < 9){
+			palavra = getPalavraAletoria();				
+			preencherDadosPalavras(palavra);
+			qntPalavrasPuladas++;	
+			$('#qnt-pontuacao').text(calcPontuacao());
+		}else{
+			$.notify('Pronto');
 		}
-		preencherDadosPalavras(palavra);
 	})
 
 	$('#limpar-texto').click(function(){
@@ -20,17 +26,31 @@ $(function(){
 
 	$('#resposta-texto').click(function(){
 		let palavraDigitada = $('#respota-texto').val();
-		if(palavra[0] == palavraDigitada){
-			$.notify("Certo", "success");
-			palavra = getPalavraAletoria();
-			preencherDadosPalavras(palavra);
-			qntAcertos++;
-			$('#qnt-acertos').text(qntAcertos);
-			$('#respota-texto').val(' ')
+		palavraDigitada = palavraDigitada.trim();
+		let qntPalavraUsadas = getQntPalavrasUtilizadas();
+		if(qntPalavraUsadas <= 10){
+			if(palavra[0] == palavraDigitada){
+				$.notify("Certo", "success");
+				palavra.splice(0,palavra.length);
+				palavra = getPalavraAletoria();
+				preencherDadosPalavras(palavra);
+				qntAcertos++;
+				$('#qnt-acertos').text(qntAcertos);
+				$('#respota-texto').val(' ');
+				$('#qnt-pontuacao').text(calcPontuacao());
+			}else{
+				$.notify('Palavra errada', 'error');
+			}
 		}else{
-			return false;
-		}		
+			$.notify('erro2');
+		}	
 	});
+
+	function calcPontuacao(){	
+		let qntAcertos_ = qntAcertos;
+		var pontuação = qntAcertos_*10 - (qntPalavrasPuladas*2);
+		return pontuação;
+	}
 })
 
 
@@ -40,7 +60,7 @@ $(function(){
 	const PalavraQuatro = ['barcelona', 'Clube de futebol famoso da Espanha', 'lo-na-ce-bar'];
 	const PalavraCinco = ['maradona', 'Jogador de futebol argentino, considerado um dos melhores da história', 'ra-ma-do-na'];
 	const PalavraSeis = ['python', 'Linguagem de programação popular para ciência de dados', 'thon-py'];
-	const PalavraSete = ['javaScript', 'Linguagem fundamental para desenvolvimento web', 't-va-scrip-ja'];
+	const PalavraSete = ['javascript', 'Linguagem fundamental para desenvolvimento web', 't-va-scrip-ja'];
 	const PalavraOito = ['sonic', 'Personagem famoso de jogos de vídeo game, conhecido como "Sonic the Hedgehog"', 'nic-so'];
 	const PalavraNove = ['daenerys', 'Personagem central da série "Game of Thrones"', 'ne-e-da-rys'];
 	const PalavraDez = ['neymar', 'Atacante brasileiro destacado no futebol mundial', 'mar-ney'];
@@ -66,24 +86,25 @@ function numeroAleatorioEntre(min, max) {
 };
 
 function getPalavraAletoria(){
-	var numeroSelecionado;
-	if(numerosUtilizados.length() > 9){
-		return undefined;
-	}else{
+	var numeroSelecionado;	
 		for (var i = 0; i < 10; i++) {
-			numeroSelecionado = numeroAleatorioEntre(0,9);
+			numeroSelecionado = numeroAleatorioEntre(0,10);
 			if(!numerosUtilizados.includes(numeroSelecionado)){
 				numerosUtilizados.push(numeroSelecionado);		
 				return palavrasCombinadas[numeroSelecionado];
 			}
 		}
-	}
 };
 
 function preencherDadosPalavras(palavra_){
 	$('#palavra-embaralhada').text(palavra_[2]);
 	$('#dica-palavra').text(palavra_[1]);
 }
+
+function getQntPalavrasUtilizadas(){
+	return numerosUtilizados.length;
+}
+
 
 
 
